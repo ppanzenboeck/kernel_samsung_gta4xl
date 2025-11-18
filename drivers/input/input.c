75fafc7d0f378eea1ab602cae53f7f5eb45090cb
Pranav Vashi <neobuddy89@gmail.com>
2025-11-18 17:50:38 +0530

    fs: kernelsu: Add scope-minimized manual hooks 1.4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -844,6 +844,11 @@ void input_booster_init(void)
  * to 'seed' initial state of a switch or initial position of absolute
  * axis, etc.
  */
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif
+
 void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
@@ -851,6 +856,10 @@ void input_event(struct input_dev *dev,
 
 	// Input Booster +
 	int idx = 0;
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+#endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
